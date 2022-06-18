1e77d1703ee87709ae12af197a65e4963147015f
Krzysztof Dereszyński <79771945+G3rw4nt@users.noreply.github.com>
2022-06-18 23:18:24 +0000

    GUI added

--- main.py
+++ main.py
@@ -8,16 +8,56 @@ import hashlib
 import PySimpleGUI as sg
 import binascii
 
-message = 'Wiadomosc'
+#wprowadzanie wiadomości i generowanie skrótu SHA
+layout = [[sg.Text('Wprowadź wiadomość, którą chcesz zaszyfrować: ')],
+                 [sg.InputText()],
+                 [sg.Submit(), sg.Cancel()]]
+
+
+window = sg.Window('Window Title', layout)
+event, values = window.read()
+window.close()
+message = values[0]
+sg.popup('Wprowadzona wiadomość: ', message)
+
 messageSHA = hashlib.sha3_224(message.encode('ascii')).hexdigest().encode('ascii')
+
+#generowanie pary kluczy
 keyPair = RSA.generate(1024)
 pubKey = keyPair.publickey()
+sg.popup('Klucz publiczny: ', pubKey.n)
+
 encryptor = PKCS1_OAEP.new(pubKey)
 encrypted = encryptor.encrypt(messageSHA)
+sg.popup('Zakodowana wiadomość: \n' + str(encrypted))
+
+layout = [[sg.Text('Jeśli chcesz zmodyfikować klucz prywatny, to zrób to teraz lub zostaw go bez zmian: ')],
+                 [sg.InputText(default_text=keyPair.export_key().decode('ascii'))],
+                 [sg.Submit(), sg.Cancel()]]
+
+window = sg.Window('Window Title', layout)
+event, values = window.read()
+window.close()
+if values[0] == keyPair.export_key().decode('ascii'):
+  sg.popup('Klucz prywatny jest poprawny,\nnastąpi teraz odszyfrowanie wiadomości.')
+else:
+  sg.popup('Klucz prywatny jest nieprawidłowy.\nProgram zakończy teraz swoje działanie')
+  sys.exit()
 
 decryptor =  PKCS1_OAEP.new(keyPair)
 decrypted = decryptor.decrypt(encrypted)
-if(messageSHA == decrypted):
+receivedMessage = message
+
+layout = [[sg.Text('Teraz możesz zmodyfikować otrzymaną wiadomość.\nJeśli nie chcesz tego robić, przejdź dalej. ')],
+                 [sg.InputText(default_text = receivedMessage)],
+                 [sg.Submit(), sg.Cancel()]]
+
+reecivedMessage = values[0]
+window = sg.Window('Window Title', layout)
+event, values = window.read()
+window.close()
+receivedMessageSHA = hashlib.sha3_224(receivedMessage.encode('ascii')).hexdigest().encode('ascii')
+if(receivedMessageSHA == decrypted):
   print("SHA correct")
 
 else:
